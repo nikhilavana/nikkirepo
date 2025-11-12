e7b44c98624de46e791c14fe49b57b9322ef9bcd
nikhilavana <nikhilavana2006@gmail.com>
2025-11-12 12:43:13 +0530

    Add program to calculate the sum of two numbers

---
+++ python.py
@@ -0,0 +1,11 @@
+# Program to find the sum of two numbers
+
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
